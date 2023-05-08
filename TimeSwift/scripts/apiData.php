ef9a6bdc4ab237f4a1c147b9ee86239454830f0b
TechSwiftXYZ <contact@techswift.xyz>
2023-05-08 10:16:33 +0100

    Version 1.1.0
    Bug fixes, improvements and new features

--- TimeSwift/scripts/apiData.php
+++ TimeSwift/scripts/apiData.php
@@ -6,26 +6,37 @@ $longitude = $_POST['longitude'];
 // Define API endpoint and parameters
 $endpoint = "https://api.weatherapi.com/v1/forecast.json";
 $params = array(
-    "key" => "api_key_redacted_for_security",
+    "key" => "api_key_removed_for_security",
     "q" => $latitude . "," . $longitude,
     "days" => "3"
 );
 
-// Create cURL handle
-$curl = curl_init();
+// Check if cache file exists and is less than 15 minutes old
+$cacheDir = dirname(__FILE__) . '/cache/';
+$cacheFile = $cacheDir . "weather_" . md5(serialize($params)) . ".json";
+if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < 900)) {
+    // Serve cached data
+    $response = file_get_contents($cacheFile);
+} else {
+    // Create cURL handle
+    $curl = curl_init();
 
-// Set cURL options
-curl_setopt($curl, CURLOPT_URL, $endpoint . '?' . http_build_query($params));
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    "Content-Type: application/json"
-));
+    // Set cURL options
+    curl_setopt($curl, CURLOPT_URL, $endpoint . '?' . http_build_query($params));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        "Content-Type: application/json"
+    ));
 
-// Execute cURL request and retrieve response
-$response = curl_exec($curl);
+    // Execute cURL request and retrieve response
+    $response = curl_exec($curl);
 
-// Close cURL handle
-curl_close($curl);
+    // Close cURL handle
+    curl_close($curl);
+
+    // Save response to cache file
+    file_put_contents($cacheFile, $response);
+}
 
 // Process response data
 $data = json_decode($response, true);
--- TimeSwift/scripts/emailSend.php
+++ TimeSwift/scripts/emailSend.php
@@ -13,14 +13,14 @@ $mail->isSMTP();
 $mail->Host = 'smtp.porkbun.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'contact@techswift.xyz';
-$mail->Password = 'mail_password_redacted_for_security';
+$mail->Password = 'email_password_removed_for_security';
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
 $mail->setFrom('contact@techswift.xyz', 'TechSwift Contact Form');
 $mail->addAddress('contact@techswift.xyz', 'TechSwift');
 $mail->Subject = "New Support Request: ".$messageSubject;
-$mail->Body = "User's name: " . $userName . "User's email address: " . $userEmail . "\nMessage:\n" . $message;
+$mail->Body = "Wesite: time.techswift.xyz" . "User's name: " . $userName . "User's email address: " . $userEmail . "\nMessage:\n" . $message;
 
 // send the message
 if(!$mail->send()){
